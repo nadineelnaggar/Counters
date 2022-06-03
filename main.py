86e4b40d94055e23fdc099a94e06df5c70425cb7
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-06-03 18:58:37 +0100

    create main file and argument parser

---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import argparse
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model',type=str)
+parser.add_argument('--train_seq_length',type=int,default=4, help='length of sequences in training set: 4, 8')
+parser.add_argument('--long_seq_length',type=int,default=30,help='length of sequences in long test set')
+
+
