6dfb52e6af21d7940d951d216d7fb7c57302baf8
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-12-14 21:10:37 +0000

    add codes for longer datasets for ternary classification

--- AdjustDatasets.py
+++ AdjustDatasets.py
@@ -40,13 +40,37 @@ def removeOversampling(dataset_name):
         with open('CounterDataset16TokensNoOversampling.txt', 'a') as f:
             for i in range(len(x)):
                 f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset10Tokens.txt':
+        with open('CounterDataset10TokensNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset20Tokens.txt':
+        with open('CounterDataset20TokensNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset30Tokens.txt':
+        with open('CounterDataset30TokensNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset40Tokens.txt':
+        with open('CounterDataset40TokensNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset50Tokens.txt':
+        with open('CounterDataset50TokensNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
 
 #
 # removeOversampling('CounterDataset2Tokens.txt')
 # removeOversampling('CounterDataset4Tokens.txt')
 # removeOversampling('CounterDataset8Tokens.txt')
 # removeOversampling('CounterDataset16Tokens.txt')
-
+# removeOversampling('CounterDataset10Tokens.txt')
+# removeOversampling('CounterDataset20Tokens.txt')
+# removeOversampling('CounterDataset30Tokens.txt')
+# removeOversampling('CounterDataset40Tokens.txt')
+# removeOversampling('CounterDataset50Tokens.txt')
 
 def makeDatasetTernary(dataset_name):
     x = []
@@ -83,6 +107,26 @@ def makeDatasetTernary(dataset_name):
         with open('CounterDataset16TokensTernary.txt', 'a') as f:
             for i in range(len(x)):
                 f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset10Tokens.txt':
+        with open('CounterDataset10TokensTernary.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset20Tokens.txt':
+        with open('CounterDataset20TokensTernary.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset30Tokens.txt':
+        with open('CounterDataset30TokensTernary.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset40Tokens.txt':
+        with open('CounterDataset40TokensTernary.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset50Tokens.txt':
+        with open('CounterDataset50TokensTernary.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
     elif dataset_name=='CounterDataset2TokensNoOversampling.txt':
         with open('CounterDataset2TokensTernaryNoOversampling.txt', 'a') as f:
             for i in range(len(x)):
@@ -99,12 +143,43 @@ def makeDatasetTernary(dataset_name):
         with open('CounterDataset16TokensTernaryNoOversampling.txt', 'a') as f:
             for i in range(len(x)):
                 f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset10TokensNoOversampling.txt':
+        with open('CounterDataset10TokensTernaryNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset20TokensNoOversampling.txt':
+        with open('CounterDataset20TokensTernaryNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset30TokensNoOversampling.txt':
+        with open('CounterDataset30TokensTernaryNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset40TokensNoOversampling.txt':
+        with open('CounterDataset40TokensTernaryNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
+    elif dataset_name=='CounterDataset50TokensNoOversampling.txt':
+        with open('CounterDataset50TokensTernaryNoOversampling.txt', 'a') as f:
+            for i in range(len(x)):
+                f.write(x[i] + ',' + y[i] + '\n')
 
 # makeDatasetTernary('CounterDataset2Tokens.txt')
 # makeDatasetTernary('CounterDataset4Tokens.txt')
 # makeDatasetTernary('CounterDataset8Tokens.txt')
-makeDatasetTernary('CounterDataset16Tokens.txt')
+# makeDatasetTernary('CounterDataset16Tokens.txt')
 # makeDatasetTernary('CounterDataset2TokensNoOversampling.txt')
 # makeDatasetTernary('CounterDataset4TokensNoOversampling.txt')
 # makeDatasetTernary('CounterDataset8TokensNoOversampling.txt')
-makeDatasetTernary('CounterDataset16TokensNoOversampling.txt')
+# makeDatasetTernary('CounterDataset16TokensNoOversampling.txt')
+
+# makeDatasetTernary('CounterDataset10Tokens.txt')
+# makeDatasetTernary('CounterDataset20Tokens.txt')
+# makeDatasetTernary('CounterDataset30Tokens.txt')
+# makeDatasetTernary('CounterDataset40Tokens.txt')
+# makeDatasetTernary('CounterDataset50Tokens.txt')
+# makeDatasetTernary('CounterDataset10TokensNoOversampling.txt')
+# makeDatasetTernary('CounterDataset20TokensNoOversampling.txt')
+# makeDatasetTernary('CounterDataset30TokensNoOversampling.txt')
+# makeDatasetTernary('CounterDataset40TokensNoOversampling.txt')
+# makeDatasetTernary('CounterDataset50TokensNoOversampling.txt')
